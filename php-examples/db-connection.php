8c6a2c3afc75fb73a0300ecfb52ac534afd1c6c5
Nippon Chowdhury <mindbend91@gmail.com>
2026-02-20 02:11:15 +0600

    Add MySQL database connection example

---
+++ php-examples/db-connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "testdb";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to MySQL database.";
+
+$conn->close();
+
+?>
