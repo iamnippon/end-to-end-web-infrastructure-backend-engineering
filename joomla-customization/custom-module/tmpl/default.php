3bc4b8b22231a5580d70d8d3ba3e3c79a378b6dc
Nippon Chowdhury <mindbend91@gmail.com>
2026-02-20 02:24:58 +0600

    Create default.php

---
+++ joomla-customization/custom-module/tmpl/default.php
@@ -0,0 +1,8 @@
+<?php defined('_JEXEC') or die; ?>
+
+<div class="backend-stats">
+    <h3>System Information</h3>
+    <p><strong>Server Time:</strong> <?php echo $data['server_time']; ?></p>
+    <p><strong>PHP Version:</strong> <?php echo $data['php_version']; ?></p>
+    <p><strong>Joomla Version:</strong> <?php echo $data['joomla_version']; ?></p>
+</div>
